998e0b1db35be6ed8f9e49d897b3f98e2c02e021
maklis <112279892+maklis@users.noreply.github.com>
2023-07-13 18:17:25 -0400

    Add files via upload

---
+++ HW5Randomized.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.Collections; 
+import java.util.Random;
+public class HW5Randomized {    
+   
+     private static int computeSum(int arr[], int n) {
+ 
+         //base or terminating condition
+         if (n <= 0) {
+            return 0;
+          }
+ 
+          //Calling method recursively
+          return computeSum(arr, n-1 ) + arr[n-1];
+       }
+   
+    public static void main(String[] args) {        
+        Random rd = new Random();
+        //define original array     
+        int [] intArray = new int [] {57,340,132,576,102,827,52,983,34,7};    
+        for (int i = 0; i < intArray.length; i++) {
+         intArray [i] = rd.nextInt();
+         } 
+        int temp = 0;    
+ 
+        //print original array    
+       System.out.println("Original array: ");    
+       for (int i = 0; i <intArray.length; i++) {     
+           System.out.print(intArray[i] + " ");    
+        }    
+        
+        //Print the sum
+        int sum = computeSum(intArray, intArray.length);
+        System.out.println("\n\nRecursive Sum: ");
+        System.out.println(sum);
+        
+        //Sort the array in ascending order using two for loops    
+        for (int i = 0; i <intArray.length; i++) {     
+          for (int j = i+1; j <intArray.length; j++) {     
+              if(intArray[i] >intArray[j]) {      //swap elements if not in order
+                 temp = intArray[i];    
+                 intArray[i] = intArray[j];    
+                 intArray[j] = temp;    
+               }     
+            }     
+        }    
+        //print sorted array    
+        System.out.println("\nArray sorted: ");    
+        for (int i = 0; i <intArray.length; i++) {     
+            System.out.print(intArray[i] + " ");    
+        }    
+    }    
+}   
